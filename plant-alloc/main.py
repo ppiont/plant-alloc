861653c8caa4a5f1797489bd536e1bc51cfdcda0
Peter Piontek <piontek0@gmail.com>
2021-01-04 12:21:09 +0100

    it's solved

--- plant-alloc/main.py
+++ plant-alloc/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 14 09:38:56 2020.
+Created on Sun Jan  3 16:39:09 2021
 
-@author: peter
+@author: peterp
 """
+
 # Import standard library
 import pathlib
 
@@ -13,13 +14,8 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Import custom libraries
-
-# Settings
-pd.options.display.max_rows = 350
-pd.options.display.max_columns = 50
-plt.style.use('seaborn-colorblind')
+import scipy.optimize as sciopt
+from scipy.spatial import distance_matrix
 
 # Set data directory
 datadir = pathlib.Path('data')
@@ -28,136 +24,98 @@ outdir = pathlib.Path('out')
 # Load files
 consumption = (gpd.read_file(datadir.joinpath('Regional_BC_2030.geojson'))
                .set_crs('EPSG:3035', allow_override=True))
-plants = (gpd.read_file(datadir.joinpath('UBC_processing_2030.geojson'))
-          .set_crs('EPSG:3035', allow_override=True))
+facs = (gpd.read_file(datadir.joinpath('UBC_processing_2030.geojson'))
+        .set_crs('EPSG:3035', allow_override=True))
 
 # Keep only relevant columns and areas within Europe proper
-regions = consumption[['NUTS_ID', 'NUTS_NAME', 'Country', 'capita',
-                       'BC per cap', 'Collecti_2', 'Color', 'BC_per_reg',
-                       'geometry']].cx[2300000:7300000, 1060000:]
+regs = consumption[['NUTS_ID', 'NUTS_NAME', 'Country', 'capita',
+                    'BC per cap', 'Collecti_2', 'Color', 'BC_per_reg',
+                    'geometry']].cx[2300000:7300000, 1060000:]
 
-# Quick visual check
-fig, ax = plt.subplots()
-regions.plot(ax=ax)
-plants.plot(color='red', ax=ax)
+# # Quick visual check
+# fig, ax = plt.subplots()
+# regs.plot(ax=ax)
+# facs.plot(color='red', ax=ax)
 
+# Number of Facilities and Regions
+N_F = len(facs)
+N_R = len(regs)
 
-def euclid_dist(a, b):
-    """Compute the Euclidean distance between points `a` and `b`."""
-    x1, y1 = a[0][0], a[1][0]
-    x2, y2 = b[0][0], b[1][0]
-    return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+# Facility capacity
+C = np.array(facs['capacity i'].astype(np.float))
+# Regional supply
+S = np.array(regs.BC_per_reg)
 
+# Coordinates of facilities and regions
+F_coords = [[x, y] for x, y in zip(facs.geometry.x, facs.geometry.y)]
+R_coords = [[x, y] for x, y in zip(regs.geometry.centroid.x,
+                                   regs.geometry.centroid.y)]
 
-def get_dists(plants, regions, plant_id='name', reg_id='NUTS_ID'):
-    """Compute the distances between sorting plants and consumption regions.
+# Distance matrix (regions = rows, facilities = cols) /1000 for km
+dist = distance_matrix(R_coords, F_coords)/1000
+# Distance vector
+dist_vec = dist.flatten()
 
-    Parameters
-    ----------
-    plants : geopandas.GeoDataFrame
-        Dataframe containing plant geometry.
-    regions : geopandas.GeoDataFrame
-        Dataframe containing region geometry.
-    plant_id : string, optional
-        Name of the column that uniquely identifies plants.
-        The default is 'name'.
-    region_id : string, optional
-        Name of the column that uniquely identifies regions.
-        The default is 'NUTS_ID'.
+# Initialize basis vector of R^(len(facilities))
+ones_N_F = np.ones(N_F)
+# Define constraints
+A_capacity = np.zeros((N_F, N_R * N_F))
+for i in range(N_R):
+    A_capacity[:, i * N_F:(i * N_F + N_F)] = np.diag(ones_N_F)
 
-    Returns
-    -------
-    plant_dists : pandas.DataFrame
-        Dataframe containing distances between plants and regions
-    """
-    # Initialize dataframe with region IDs as index
-    plant_dists = pd.DataFrame(index=regions[reg_id])
-    # Iterate over plants
-    for _, plant in plants.iterrows():
-        # Iterate over regions
-        for _, region in regions.iterrows():
-            # Compute and add distance between plant and region
-            plant_dists.loc[region[reg_id], plant[plant_id]] = (
-                euclid_dist(plant.geometry.xy, region.geometry.centroid.xy))
-    return plant_dists
+A_supply = np.zeros((N_R, N_R * N_F))
+for i in range(N_R):
+    A_supply[i, i * N_F:(i * N_F + N_F)] = ones_N_F
 
+# Create solver
+x_opt = sciopt.linprog(c=dist_vec, A_eq=A_capacity, b_eq=C,
+                       A_ub=A_supply, b_ub=S, bounds=(0, None),
+                       method='interior-point')
 
-def allocate_recycling(plants, regions, plant_id='name', reg_id='NUTS_ID',
-                       capacity_col='capacity i', consump_col='BC_per_reg'):
-    """Allocate regional beverage carton streams to processing plants.
+# Output solution to matrix
+x_opt_mat = np.reshape(x_opt.x, (N_R, N_F))
+# Normalize matrix for volume weighted plotting of supply lines
+x_opt_mat_normalized = x_opt_mat/np.linalg.norm(x_opt_mat)
 
-    Allocates on the basis of plant capacity and regional volume,
-    always taking proximity into account.
+# Create matrix of solution variables
+out = pd.DataFrame(x_opt_mat)
+# Get facility names/columns
+fac_cols = list(facs.name)
+# Add facility names to solution matrix cols
+out.columns = fac_cols
+# Add region names to solution matrix rows
+out.insert(loc=0, column='NUTS_ID', value=regs.NUTS_ID)
 
-    Parameters
-    ----------
-    plants : geopandas.GeoDataFrame
-        Dataframe containing plant geometry.
-    regions : geopandas.GeoDataFrame
-        Dataframe containing region geometry.
-    plant_id : string, optional
-        Name of the column that uniquely identifies plants.
-        The default is 'name'.
-    region_id : string, optional
-        Name of the column that uniquely identifies regions.
-        The default is 'NUTS_ID'.
-    capacity_col : string, optional
-        Name of the column that stores processing plant capacity.
-        The default is 'capacity i'.
-    consump_col : string, optional
-        Name of the column that stores regional beverage carton consumption.
-        The default is 'BC_per_reg'.
+# Assign regions to facility they allocate their largest share to
+out['Assignment'] = np.where(out[fac_cols].sum(axis=1) <= 0.01, 'unassigned',
+                             out[fac_cols].idxmax(axis=1))
 
-    Returns
-    -------
-    output : geopandas.GeoDataFrame
-        A copy of the `regions` input df with an added column that designates
-        which plant a region is assigned to.
-    """
-    # Get distances between plants and regions
-    distances = get_dists(plants, regions, plant_id=plant_id,
-                          reg_id=reg_id)
-    # Copy regions to make output file without modifying original
-    output = regions.copy()
-    # Initialize allocation column
-    output['plant_alloc'] = 'unallocated'
-    # Create set to store visited regions
-    visited = set()
-    # Iterate over plants
-    for _, plant in plants.iterrows():
-        # Read capacity
-        capacity = int(plant[capacity_col])
-        # Read distance to all regions, sorted ascending
-        reg_dist = distances[[plant[plant_id]]].sort_values(plant[plant_id])
-        # Cycle distances from nearest to farthest
-        for reg_idx, row in reg_dist.iterrows():
-            # Extract beverage carton consumption from regions df
-            consumption = np.float(
-                regions[regions[reg_id] == reg_idx][consump_col])
-            if reg_idx in visited:
-                # Move on if the region has already been allocated
-                continue
-            elif capacity > 0:
-                # In case of sufficient capacity
-                # get index of currently looping region in output file
-                out_idx = output[output[reg_id] == reg_idx].index[0]
-                # Subtract consumed capacity
-                capacity -= consumption
-                # Assign region to plant and add to visited set
-                output.loc[out_idx, 'plant_alloc'] = plant[plant_id]
-                visited.add(reg_idx)
-            else:
-                # Stop loop when plant capacity has been reached
-                break
-
-    return output
+# Output solution df to csv
+out.to_csv(outdir.joinpath('solution.csv'))
 
 
-# Run and save
-out = allocate_recycling(plants, regions)
+# PLOT STUFF
+# fig, ax = plt.subplots(figsize=(15, 15))
+# regs.plot(ax=ax)
+# facs.plot(color='red', ax=ax)
 
-fig, ax = plt.subplots(figsize=(15, 15))
-out.plot(column='plant_alloc', ax=ax)
-plants.plot(color='black', ax=ax)
+# for i in range(N_R):
+#     ax.annotate(f'{S[i]-np.sum(x_opt_mat[i,:]):.1f}',
+#                 (regs.iloc[i, :].geometry.centroid.x,
+#                  regs.iloc[i, :].geometry.centroid.y), fontsize=8)
 
-out.to_file(outdir.joinpath('regions_w_plant_alloc.geojson', driver='GeoJSON'))
+#     for j in range(N_F):
+#         plt.plot([regs.iloc[i, :].geometry.centroid.x,
+#                   facs.iloc[j, :].geometry.x],
+#                  [regs.iloc[i, :].geometry.centroid.y,
+#                   facs.iloc[j, :].geometry.y], 'k-',
+#                  linewidth=x_opt_mat_normalized[i, j] * 10)
+#         if x_opt_mat_normalized[i, j] > 1e-8:
+#             ax.annotate("{:.1f}".format(x_opt_mat[i, j]),
+#                         ((regs.iloc[i, :].geometry.centroid.x +
+#                           facs.iloc[j, :].geometry.x)/2,
+#                          (regs.iloc[i, :].geometry.centroid.y +
+#                           facs.iloc[j, :].geometry.y)/2),
+#                         fontsize=15)
+# plt.savefig('test.svg')
+# plt.show()
